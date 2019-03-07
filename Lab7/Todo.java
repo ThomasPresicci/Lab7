95268c69188eb1da08355ec70e7ede72ea00735c
Thomas Presicci <thomaspresicci@gmail.com>
2019-03-07 13:02:16 -0600

    Completed tasks and estimates

---
+++ Lab7/Todo.java
@@ -0,0 +1,27 @@
+public class Todo {/*
+Lab Section_________: 12
+Last Modified_______: 2019-03-07
+OU ID_______________: 113457929
+OU EMAIL____________: thomaspresicci@ou.edu
+
+Tasks:
+1. Implement PriorityQueueHospital.java
+2. Implement QueueHospital.java
+3. Implement StackHospital.java
+4. Implement SickPerson.java and HealthyPerson.java
+5. Implement testing for all classes
+
+Estimated Time (minutes):
+1. 20
+2. 15
+3. 15
+4. 25
+5. 30
+
+Actual Time (minutes):
+1. 
+2. 
+3. 
+4. 
+5. 
+*/}
